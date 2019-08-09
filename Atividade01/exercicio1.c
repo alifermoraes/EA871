271d238690cf29c7a306aa58596f7a97952efab2
Alifer Moraes <alifer.wsdm@gmail.com>
2019-08-09 16:15:14 -0300

    First commit

---
+++ Atividade01/exercicio1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	/* printf("coisa a imprimir") imprime algo na tela.
+	Alguns caracteres especiais podem ser uteis para formatacao:
+	\n (newline) representa o fim de uma linha.
+	\t (tabulation) representa uma tabulacao.
+	\\ (barra invertida) imprime uma barra invertida.
+	*/
+
+	printf("Estou funcionando!\n");
+	return 0;
+}
