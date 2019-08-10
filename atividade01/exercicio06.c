f66f3cd3bc88656e8a3d74b38919646444e51a43
Alifer Moraes <alifer.wsdm@gmail.com>
2019-08-10 13:43:00 -0300

    Add exercicio06.c

---
+++ atividade01/exercicio06.c
@@ -0,0 +1,27 @@
+/*
+ * Analise o programa abaixo.
+ * a) O que as operações &, | e << fazem?
+ * b) O que a notação 0x... significa?
+ * c) O que deverá ser impresso na tela?
+ * d) Entre com o programa em seu editor preferido, compile e execute. Sua hipótese se confirmou?
+ * e) Modifique as operações (e/ou remova alguma, se preferir) para que somente os dois bits mais
+ * significativos de c sejam iguais a zero, e os outros sejam iguais a 1.
+ */
+
+#include <stdio.h>
+
+int main() {
+    char c = 0xF3;
+
+    /*
+     * c = c & 0x01;
+     * c = c << 3;
+     * c = c | 0x06;
+     */
+
+    c &= 0x00;
+    c |= 0x3F;
+
+    printf("%d\n", c);
+    return 0;
+}
