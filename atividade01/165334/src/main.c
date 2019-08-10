8838d8d6ee229e627869ca708dfd4ee08bdd1eb3
Alifer Moraes <alifer.wsdm@gmail.com>
2019-08-10 15:16:18 -0300

    Add homework (165334)

---
+++ atividade01/165334/src/main.c
@@ -0,0 +1,31 @@
+/* Contador de bits
+ *
+ * Este programa conta o numero de bits em um byte
+ */
+
+#include <stdio.h>
+
+int main() {
+
+  unsigned char entrada;
+  unsigned int tmp;
+  unsigned int n_bits;
+
+  /* Ler entrada em hexadecimal */
+  scanf("%x", &tmp);
+  entrada = (unsigned char)tmp;
+
+  n_bits = 0;
+
+  while (tmp != 0x00) {
+      if ((tmp | 0x01) == tmp) { /* Checa se o bit menos significativo de tmp é igual à 1. Caso seja, adiciona 1 à contagem de bits */
+          n_bits++;
+      }
+
+      tmp >>= 1;
+  }
+
+  /* Escrever numero de bits */
+  printf("%d\n", n_bits);
+  return 0;
+}
