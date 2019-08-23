90b9f55318a36005e19e83df38dc00b356d6627b
Alifer Moraes <alifer.wsdm@gmail.com>
2019-08-23 17:44:19 -0300

    Update exercicio02.c

--- atividade03/exercicio02.c
+++ atividade03/exercicio02.c
@@ -15,29 +15,31 @@
  * desejado.
  */
 
+#include <avr/io.h>
 #include <stdint.h>
 
+uint8_t * ptr_portc = (uint8_t *) 0x28;
+uint8_t * ptr_ddrc = (uint8_t *) 0x27;
+uint8_t * ptr_pind = (uint8_t *) 0x29;
+uint8_t * ptr_ddrd = (uint8_t *) 0x2A;
+uint8_t pin_d7_mask = 0x80;
+uint8_t pin_c0_mask = 0x01;
+uint8_t portd_state;
+
 int main(void) {
-    uint8_t * ptr_portc = (uint8_t *) 0x28;
-    uint8_t * ptr_ddrc = (uint8_t *) 0x27;
-    uint8_t * ptr_portd = (uint8_t *) 0x2B;
-    uint8_t * ptr_ddrd = (uint8_t *) 0x2A;
-    uint8_t pin_d7_mask = 0x08;
-    uint8_t pin_c0_mask = 0x01;
-    uint8_t portd_state;
 
-    /* Seta o bit 0 do portc como saida */
-    *ptr_ddrc |= 0x01;
-    /* Seta o bit 7 do portd como entrada */
-    *ptr_ddrd |= 0x08;
+	/* Seta o bit 0 do portc como saida */
+	*ptr_ddrc |= 0x01;
+	/* Seta o bit 7 do portd como entrada */
+	*ptr_ddrd &= 0x7F;
+	
 
-    while (1) {
-        portd_state = *ptr_portd;
 
-        if (portd_state & pin_d7_mask) {
-            *ptr_portc &= ~pin_c0_mask;
-        } else {
-            *ptr_portc |= pin_c0_mask;
-        }
-    }
+	while (1) {
+		if (*ptr_pind & pin_d7_mask) {
+			*ptr_portc &= ~pin_c0_mask;
+		} else {
+			*ptr_portc |= pin_c0_mask;
+		}
+	}
 }
