2f28eb8671ec70ebd9b8d23a2e1c9f98f144feca
Alifer Moraes <alifer.wsdm@gmail.com>
2019-09-25 17:06:46 -0300

    Update main.asm

--- atividade04/atividade4/atividade4/main.asm
+++ atividade04/atividade4/atividade4/main.asm
@@ -1,5 +1,5 @@
 /**
- * Atividade03 - GPIO
+ * Atividade04 - Assembly
  *
  * Alifer Willians de Moraes
  * RA: 165334
@@ -10,7 +10,7 @@
  * C do Atmel Studio. No caso, a sub-rotina deve receber como entrada um valor de 16 bits (sem
  * sinal) que especifica o tempo de atraso em milissegundos (e.g., um valor igual a 1000 deve levar
  * a rotina a produzir um atraso de 1 segundo). Este valor deve ser recebido pela sub-rotina
- * através do par de registradores r25:r24.
+ * através do par de registradores R25:R24.
  * Por fim, escreva um programa em Assembly que faça com que um LED pisque com um período de 1
  * segundo. Ou seja, o LED deve permanecer aceso por 0,5 segundo, ficando apagado por 0,5 segundo em
  * seguida. Por simplicidade, vamos utilizar o próprio LED incorporado presente no Arduino UNO, o
@@ -30,11 +30,11 @@ JMP MAIN
 .ORG 0x0034
 
 MAIN:
-    /* Define o valore de R25:R24 para simular o parâmetro que será passado pelo programa em C */
+    /* Define o valore de R25:R24 para simular o parâmetro que será passado pelo programa em C (500) */
     LDI PRMLO, 0xF4
     LDI PRMHI, 0x01
 
-    /* Seta o portb como saida */
+    /* Seta o pino 5 do portb como saída */
     LDI BMASK, 0x20
     OUT DDRB, BMASK
 
@@ -54,12 +54,13 @@ DELAY:
 
     /*
      * Necessário incrementar R25 para que o delay base seja executado (R24 + 256 * R25), pois essa
-     * sub brotina executa R24 + 256 * (R25 -1) vezes. (Tratando R24 e R25 como valores
+     * sub-rotina executa R24 + 256 * (R25 - 1) vezes. (Tratando R24 e R25 como valores
      * independentes de 8 bits).
      */
     INC PRMHI
 
-    /* Valores iniciais para um atraso base de aproximadamente 16_000 ciclos
+    /* 
+     * Valores iniciais para um atraso base de aproximadamente 16_000 ciclos
      * Número total de ciclos da rotina implementada: ((R17 - 1) * 3 + 2) + ((254 * 3 + 2) + 3) * (R18 - 1)
      * R17 = 219 e R18 = 21 => 15996 ciclos + 2 ciclos de LDI = 15998 ciclos.
      */
