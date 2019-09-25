71ceda72c93bf80a295e654374050de5d008f02f
Alifer Moraes <alifer.wsdm@gmail.com>
2019-09-25 17:09:11 -0300

    Update delay.s

--- atividade05/atividade5/atividade5/delay.s
+++ atividade05/atividade5/atividade5/delay.s
@@ -5,8 +5,8 @@
  * Alifer Willians de Moraes
  * RA: 165334
  *
- * Subrotina para gerar um atraso variável de acordo com o parâmetro passado pelo programa em C
- * O atraso é baseado em um atraso base de 16_000 ciclos, equivalentes a 1 segundo em
+ * Sub-rotina para gerar um atraso variável de acordo com o parâmetro passado pelo programa em C
+ * O atraso é baseado em um atraso base de 16_000 ciclos, equivalentes a 1 milisegundo em
  * microcontroladores de 16 MHz. Esse atraso base é repetido N vezes, onde N é o parâmetro passado
  * pelo programa em C (registradores R24 (LOW) e R25 (HIGH)).
  */ 
@@ -21,16 +21,19 @@ DELAY:
     PUSH R25
 
     /*
-	 * Necessário incrementar R25 para que o delay base seja executado (R24 + 256 * R25), pois essa
-	 * sub brotina executa R24 + 256 * (R25 -1) vezes. (Tratando R24 e R25 como valores
-     * independentes de 8 bits).
-	 */
+     * Necessário incrementar R25 para que o delay base seja executado (R24 + 256 * R25), pois essa
+     * sub-rotina executa R24 + 256 * (R25 - 1) vezes. (Tratando R24 e R25 como valores
+     * independentes de 8 bits)
+     */
+     
     INC R25
 
-    /* Valores iniciais para um atraso base de aproximadamente 16_000 ciclos
+    /*
+     * Valores iniciais para um atraso base de aproximadamente 16_000 ciclos
      * Número total de ciclos da rotina implementada: ((R16 - 1) * 3 + 2) + ((254 * 3 + 2) + 3) * (R17 - 1)
      * R16 = 219 e R17 = 21 => 15996 ciclos + 2 ciclos de LDI = 15998 ciclos.
      */
+
 BASE_DELAY:
     LDI R16, 219
     LDI R17, 21
