a3a534632bff52a09fc866065944cb365d6baa88
Alifer Moraes <alifer.wsdm@gmail.com>
2019-10-02 10:49:28 -0300

    Update atividade3

--- atividade03/atividade3/atividade3/main.c
+++ atividade03/atividade3/atividade3/main.c
@@ -16,28 +16,34 @@
 #include "digits.h"
 #include <util/delay.h>
 
-/* Ponteiros para os enderecos que sera usados */
+/* Ponteiros para os enderecos que serao usados */
 uint8_ptr ucsr0b = (uint8_ptr) 0xC1,
 		  portd = (uint8_ptr) 0x2B,
           ddrd = (uint8_ptr) 0x2A,
-          pinc = (uint8_ptr) 0x26,
-          ddrc = (uint8_ptr) 0x27;
+		  portc = (uint8_ptr) 0x28,
+          ddrc = (uint8_ptr) 0x27,
+          pinc = (uint8_ptr) 0x26;
 
 uint8_t pin_c0_mask = 0x01,
 		actual_state = 0x00,
 		previous_state;
+		
+void startup(void) {
+	*ucsr0b &= 0xF7; /* Desabilita a funcao UART do pino 1 do port D (TX) */
+	*ddrd |= 0xFE; /* Seta os pinos 1-7 do portd como saidas */
+	*ddrc &= 0xFE; /* Seta o pino 0 do portc como entrada */
+	*portc |= 0x01; /* Habilita o resistor de pull-up do pino 0 do port C */
+
+	/* Inicia o display exibindo o valor zero */
+	*portd &= 0x01; /* Apaga os segmentos do display */
+	*portd |= ZERO; /* Seta os bits dos segmentos referentes ao digito 0 */
+}
 
 int main(void)
-{
-	*ucsr0b &= 0xF7; /* Desabilita a funcao UART do pino 1 do port D (TX) */
-    *ddrd |= 0xFE; /* Seta os pinos 1-7 do portd como saidas */
-    *ddrc &= 0xFE; /* Seta o pino 0 do portc como entrada */
+{	
+	unsigned int counter = 0;
 
-    /* Inicia o display exibindo o valor zero */
-    *portd &= 0x01; /* Apaga os segmentos do display */
-    *portd |= ZERO; /* Seta os bits dos segmentos referentes ao digito 0 */
-	
-	int counter = 0;
+	startup();
 
     while (TRUE)
     {
@@ -52,7 +58,7 @@ int main(void)
 		    /* Debouncing */
 			_delay_ms(50);
 		    actual_state = *pinc & pin_c0_mask;
-
+			
 			/* Se o botao foi realmente pressionado, incrementa a contagem */
 		    if (!actual_state) {
 			    counter++;
